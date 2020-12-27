19f2b0300a398af834a327874cb19abe4ed642d5
allenwind <allenwind@foxmail.com>
2020-12-27 15:53:26 +0800

    Add SkipGram

---
+++ model_skipgram.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+physical_devices = tf.config.experimental.list_physical_devices("GPU")
+assert len(physical_devices) > 0, "Not enough GPU hardware devices available"
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import *
